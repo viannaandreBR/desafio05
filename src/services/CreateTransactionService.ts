dd5b4de76506ab9cbb4bb4704e680235812e5102
viannaandreBR <vianna_andre@hotmail.com@users.noreply.github.com>
2020-08-26 18:45:20 -0300

    Teste05-OK

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,13 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+
+interface Request {
+  title: string;
+  value: number;
+  type: "income" | "outcome";
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +15,21 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title, value , type }: Request ): Transaction {
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value ) {
+      throw new Error('You do not have enough balance');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type
+    });
+
+    return transaction;
   }
 }
 
